685d9d2e1f86f33d97ce95f9fd467ffeb5ff1490
Rachel-css <rmaynie@icstars.org>
2025-10-17 19:00:53 -0500

    RachelMaynie

---
+++ Developer Logs/script.js
@@ -0,0 +1,31 @@
+let username = "jessica";
+let number = "6";
+console.log(number );
+let numbers = 6;
+let x = number + numbers;
+console.log(x)
+let one = 1;
+let z = numbers+ one;
+console.log(z);
+
+let oneword = "jessica";
+let twoword= "Morton";
+let cto = oneword + "" + twoword;
+console.log(cto);
+
+function add(a,b){
+let a = 22;
+
+let b = 10;
+let c = a +b;
+console.log(sum)
+}
+Let x = "5"
+console.logic(x ==5);
+let = "false";
+let word = "false"
+let anotherWord 
+console.log("false"===false);
+Function add(){
+    
+}
---
+++ script.js
@@ -0,0 +1,31 @@
+let username = "jessica";
+let number = 6;
+console.log(number );
+
+let numbers = 6;
+let x = number + numbers;
+console.log(x);
+
+let one = 1;
+let z = numbers+ one;
+console.log(z);
+
+let oneword = "jessica";
+let twoword= "Morton";
+let cto = oneword + "" + twoword;
+console.log(cto);
+
+function add(a = 22, b = 10){
+    const sum = a + b;
+    console.log
+    return sum;
+}
+
+add(); // prints 32
+
+console.log(x === 5); // strict comparison -> false
+
+let isFalse = false;
+let word = "false";
+let anotherWord;
+console.log("false" === false); //false
